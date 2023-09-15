875a3703b497b767269696fabd586d3f6f9f5e6b
carlhiramyaro <carlhiramyaro@outlook.com>
2023-09-15 13:51:05 -0400

    initial commit.

--- SpotOn/App.js
+++ SpotOn/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This land has been claimed by aliens</Text>
+      <Text>Playlists coming soon</Text>
       <StatusBar style="auto" />
     </View>
   );
